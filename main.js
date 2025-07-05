344ed8f1cf204960520b446b22c6c61662525e76
Imtiaz Uddin <imtiazshakil203@gmail.com>
2025-07-06 04:32:59 +0600

    Fixed all button

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // selector 
 const appDivElement = document.querySelector('#app')
-const playerScorrElement = document.querySelector('.player-score')
-const computerScorrElement = document.querySelector('.computer-score')
+const playerScoreElement = document.querySelector('#player-score')
+const computerScoreElement = document.querySelector('#computer-score')
 const scoreBoardElement = document.querySelector('.score-board')
 const resultAppElement = document.querySelector('#result')
 const choicBtnElement = document.querySelector('.choice-btn')
@@ -15,32 +15,65 @@ let playerScore = 0
 let computerScore = 0
 
 // coputer random choice 
-function getCoputerChoice() {
+function getComputerChoice() {
     const choices = ['paper', 'Rock', 'Scissors']
     const randomNumber = Math.floor(Math.random() * 3)
     return choices[randomNumber]
 }
 
-// user choice 
-function getPlayerChoice(playerChoice) {
-    const computerChoice = getCoputerChoice()
+// winner functionality 
+function win(playerChoice, computerChoice, buttonElement) {
+    playerScore++
+    playerScoreElement.textContent = playerScore
+    computerScoreElement.textContent = computerScore
+    resultAppElement.textContent = `${playerChoice} beats ${computerChoice}. You Win! 🔥`
+    buttonElement.classList.add('winner')
+    setTimeout(() => {
+    buttonElement.classList.remove('winner');
+    }, 1000);
+}
+
+// lose functionality 
+function lose(playerChoice, computerChoice, buttonElement) {
+    computerScore++
+    playerScoreElement.textContent = playerScore
+    computerScoreElement.textContent = computerScore
+    resultAppElement.textContent = `${playerChoice} loses to ${computerChoice}. You Lost...😢`
+    buttonElement.classList.add('loser')
+    setTimeout(() => {
+    buttonElement.classList.remove('loser');
+    }, 1000);
+}
+
+// draw functionality 
+function draw(playerChoice, computerChoice, buttonElement) { 
+    resultAppElement.textContent = `${playerChoice} equels ${computerChoice}. It's a Draw:💤`
+    buttonElement.classList.add('draw')
+    setTimeout(() => {
+    buttonElement.classList.remove('draw');
+    }, 1000);
+}
+
+// player choice and game rules fixed
+function getPlayerChoice(playerChoice, buttonElement) {
+    const computerChoice = getComputerChoice()
     switch ( playerChoice + computerChoice) {
         case 'RockScissors':
         case 'PaperRock':
         case 'ScissorsPaper':
-            console.log('Player win!')
+            win(playerChoice, computerChoice, buttonElement)
             break;
 
         case 'RockPaper':
         case 'PaperScissors':
         case 'ScissorsRock':
-            console.log('Player loses!')
+            lose(playerChoice, computerChoice,  buttonElement)
             break;
 
         case 'RockRock':
         case 'PaperPaper':
         case 'ScissorsScissors':
-            console.log('Game is draw!')
+            draw(playerChoice, computerChoice,  buttonElement)
             break;
     }
 }
@@ -48,18 +81,18 @@ function getPlayerChoice(playerChoice) {
 function main() {
     // Rock Buttom fixed 
     rockBtnElement.addEventListener('click', () => {
-        getPlayerChoice('Rock')
-})
+        getPlayerChoice('Rock', rockBtnElement)
+    })
 
-// Paper Button fixed
+    // Paper Button fixed
     paperBtnElement.addEventListener('click', () => {
-         getPlayerChoice('Paper')
-})
+         getPlayerChoice('Paper', paperBtnElement)
+    })
 
-// Scissors Button fixed
+    // Scissors Button fixed
     scissorsBtnElement.addEventListener('click', () => {
-        getPlayerChoice('Scissors')
-})
+        getPlayerChoice('Scissors', scissorsBtnElement)
+    })
 }
 
 main()
